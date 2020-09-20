9330205eb21815c537258b0ff17132ad8106d964
audrasjb <audrasjb@gmail.com>
2020-09-20 15:35:11 +0200

    Header: fix conditional statement and improve accessibility

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -14,8 +14,8 @@ function kawi_site_branding() {
 		<div class="site-branding">
 			<?php the_custom_logo(); ?>
 			<div class="site-details">
-				<?php if ( is_front_page() ) : ?>
-					<h1 class="site-title caps"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php if ( is_front_page() || ( is_front_page() && is_home() ) ) : ?>
+					<h1 class="site-title caps"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" aria-current="page"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php else : ?>
 					<p class="site-title caps"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php endif;
