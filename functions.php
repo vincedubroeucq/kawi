39c6f077fee09999ecb0eaeb68a9550b58d32fb8
Vincent Dubroeucq <vincent@vincentdubroeucq.com>
2025-02-12 10:30:04 +0100

    fixed header meta link visited color

--- functions.php
+++ functions.php
@@ -235,7 +235,7 @@ function kawi_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'kawi_content_width', 900 );
+	$GLOBALS['content_width'] = apply_filters( 'kawi_content_width', 800 );
 }
 
 
